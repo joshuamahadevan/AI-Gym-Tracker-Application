0d9dd69810e0daef1c281258e57f6014b21f13bd
Joshua Mahadevan <joshuamahadevan1@gmail.com>
2023-09-29 12:21:44 +0530

    [feat]: moved video capture and analysis to front-end

---
+++ AI-Gym-Tracker-Code/static/js/gym_tracker.js
@@ -0,0 +1,181 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+const startButton = document.getElementById("start_button")
+
+const repOutput = document.getElementById( "rep_count" );
+const visOutput = document.getElementById( "visibility" );
+const stageOutput = document.getElementById( "stage" );
+const timerElement = document.getElementById("timer");
+
+let visible = true,
+    stage = 'down',
+    counter = 0,
+    target_count = -1;
+
+// pose identification - callback function
+function onResults(results) {
+  if (!results.poseLandmarks) return;
+
+  landmarks = results.poseLandmarks;
+
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+  // Only overwrite missing pixels.
+  canvasCtx.globalCompositeOperation = 'destination-atop';
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  canvasCtx.globalCompositeOperation = 'source-over';
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                 {color: '#00FF00', lineWidth: 4});
+  drawLandmarks(canvasCtx, results.poseLandmarks,
+                {color: '#FF0000', lineWidth: 2});
+
+  // angle calculations
+  
+  // function to calculate angles
+  const calculateAngle = ( a, b, c ) => {
+    // Convert input points to arrays
+    a = Array.isArray(a) ? a : [a[0], a[1]];
+    b = Array.isArray(b) ? b : [b[0], b[1]];
+    c = Array.isArray(c) ? c : [c[0], c[1]];
+
+    // Calculate the angle in radians
+    const radians = Math.atan2(c[1] - b[1], c[0] - b[0]) - Math.atan2(a[1] - b[1], a[0] - b[0]);
+
+    // Convert radians to degrees
+    let angle = Math.abs((radians * 180.0) / Math.PI);
+
+    // Ensure the angle is between 0 and 180 degrees
+    if (angle > 180.0) {
+      angle = 360 - angle;
+    }
+
+    return angle;
+  }
+
+  const LEFT_SHOULDER = 11,
+  LEFT_ELBOW = 13,
+  LEFT_WRIST = 15,
+  VISIBILITY_THRESHOLD = 0.5;
+
+  if( landmarks[LEFT_SHOULDER]['visibility'] < VISIBILITY_THRESHOLD || 
+      landmarks[LEFT_ELBOW]['visibility'] < VISIBILITY_THRESHOLD || 
+      landmarks[LEFT_WRIST]['visibility'] < VISIBILITY_THRESHOLD
+   ){
+      visible = false;
+    }
+  else 
+  {
+        visible = true;
+        shoulder = [landmarks[LEFT_SHOULDER].x,landmarks[LEFT_SHOULDER].y];
+        elbow = [landmarks[LEFT_ELBOW].x,landmarks[LEFT_ELBOW].y];
+        wrist = [landmarks[LEFT_WRIST].x,landmarks[LEFT_WRIST].y];
+
+        angle = calculateAngle( shoulder, elbow, wrist );
+
+        console.log( angle );
+
+
+        // TODO : more robust countinng system
+        if (angle > 160 && stage == "up") {
+        stage = "down";
+        } 
+        else if (angle < 60 && stage =="down") {
+        stage = "up";
+        counter++;
+        }
+
+        if (counter == target_count) {
+            stopTimer()
+            camera.stop()
+        }
+        // update values in the front-end
+        repOutput.innerHTML = `rep count : ${counter}`;
+        stageOutput.innerHTML = `stage : ${stage}`;
+    }
+    visOutput.innerHTML = `visible : ${visible}`;
+
+  canvasCtx.restore();
+}
+
+const pose = new Pose({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+}});
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  enableSegmentation: true,
+  smoothSegmentation: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+pose.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await pose.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+
+// auto start camera
+// -------debug only-------------
+// camera.start()
+
+
+startButton.onclick = () => {
+  target_count = document.getElementById("target_count").value;
+  visible = true;
+  stage = 'down';
+  counter = 0;
+  camera.start();
+  startTimer();
+}
+
+let seconds = 0; // Initial value in seconds
+let timerInterval; // Interval ID for the timer
+
+function stopTimer() {
+  // Clear the timer interval to stop the timer
+  console.log( seconds )
+  fetch("http://127.0.0.1:5000/exercise_data", {
+    method: "POST",
+    body: JSON.stringify({
+        time: seconds,
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+  }); 
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  resetTimer();
+
+  // Update the timer every second (1000 milliseconds)
+  timerInterval = setInterval(() => {
+    seconds++;
+    displayTime();
+  }, 1000);
+}
+
+function resetTimer() {
+  // Stop the timer and reset the seconds counter
+  stopTimer();
+  seconds = 0;
+  displayTime();
+}
+
+function displayTime() {
+  // Calculate minutes and seconds from the total seconds
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  // Display the time in the timerElement
+  timerElement.textContent = `time : ${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
